b56d1a78549dfaf9fc762828b437efea6cb3e29d
pradeepkbr07 <164230792+pradeepkbr07@users.noreply.github.com>
2024-03-21 23:34:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,4 @@
 #   Keep this window open while we are preparing your
 #   codespace. You can now navigate back to your VIKTOR
 #   environment to complete the onboarding process.
+viktor-cli create-app --app-type editor
